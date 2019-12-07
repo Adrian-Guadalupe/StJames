4deba252d24674cb65b1bbcde426f15bdfda334b
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-12-07 15:29:22 -0500

    some more temp styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 function NavBar() {
   return (
     <div className="nav-bar">
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/post-read-interview'>Interview</Link>
+      <div className='links'>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/post-read-interview'>Interview</Link>
+      </div>
     </div>
   );
 }
